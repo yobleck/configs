aa61182d233d0b857b050a1ecf2c4ae4391f35e0
yobleck <sennewell@gmail.com>
2022-03-06 10:19:32 -0500

    added uptime check
    stops locker from activating shortly after startup due to differences in ntp and cmos time

--- qtile/locker.sh
+++ qtile/locker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-if [ $(grep -r "RUNNING" /proc/asound | wc -l) -eq 0 ];
+#echo auto >> /home/yobleck/xtest.txt
+#if [ $(cut -f1 -d. /proc/uptime) -lt 500 ];
+#then
+if [ $(grep -r "RUNNING" /proc/asound | wc -l) -eq 0 ] && [ $(cut -f1 -d. /proc/uptime) -gt 500 ];
 then
 xset s activate
-i3lock -n --pass-media-keys --timecolor=00ff00 --datecolor=00ff00 --blur 5 --clock --timestr="%H:%M" --datestr="%Y-%m-%d"
+i3lock -n --pass-media-keys --time-color=00ff00 --date-color=00aa00 --blur 5 --clock --time-str="%H:%M" --date-str="%Y-%m-%d"
 xset s off
 fi
+#fi
