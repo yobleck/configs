0cff4bdec881ba5608d2b4f9dfcef7ef785b6467
yobleck <sennewell@gmail.com>
2022-09-01 18:23:46 -0400

    comment out logging

--- qtile/window_preview.sh
+++ qtile/window_preview.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-import -window $1 -geometry 320x180 /home/yobleck/.config/qtile/window_preview_images/$1.png \
-&>> /home/yobleck/.config/qtile/qtile_log.txt
+#sleep $2
+import -window $1 -geometry 320x180 /home/yobleck/.config/qtile/window_preview_images/$1.png #\
+#&>> /home/yobleck/.config/qtile/qtile_log.txt
