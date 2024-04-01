d9f3ee181a26efc482d0d34fdbc41b8d36c26ec4
yobleck <sennewell@gmail.com>
2024-03-31 20:46:38 -0400

    Create screen_locker.py
    pseudo screen locker using Qtile's popup windows

---
+++ qtile/screen_locker.py
@@ -0,0 +1,104 @@
+"""Pseudo screen locker that uses Qtile a popup window"""
+
+from datetime import datetime
+
+from libqtile import qtile, images
+from libqtile.popup import Popup
+
+
+def log_test(i):
+    f = open("/home/yobleck/.config/qtile/qtile_log.txt", "a")
+    f.write(str(i) + "\n")
+    f.close()
+
+
+class lock_screen:  # TODO turn this into an extension?
+    def __init__(
+            self, image_path: str = "", background: str = "#000000ff", foreground: str = "#ffffff",
+            font: str = "sans", font_size: int = 16,
+            fake_password: str = "", text_pos: tuple = (0, 0)) -> None:
+
+        self.image_path = image_path
+        if self.image_path is None:  # TODO ensure valid image
+            pass
+        self.background: str = background
+        self.foreground: str = foreground
+        self.font: str = font
+        self.font_size: int = font_size
+
+        self.time: str = datetime.now().isoformat()
+        self.prompt: str = "\nFake Password:"
+        self.fake_password: str = fake_password
+        self.user_input: str = ""
+        self.text_pos: tuple = text_pos
+
+        self.popups: list = []
+
+    def lock(self, unused_qtile) -> None:
+        if not self.popups:
+            for x, scrn in enumerate(qtile.screens):
+                self.popups.append(Popup(
+                    qtile, background=self.background, foreground=self.foreground,
+                    x=scrn.x, y=scrn.y,
+                    width=scrn.width, height=scrn.height,
+                    font=self.font, font_size=self.font_size, border="#ff00ff", border_width=0, opacity=1, wrap=True))
+
+                self.popups[x].unhide()
+
+                self.popups[x].win.process_button_click = self._click_popup
+                self.popups[x].win.process_key_press = self._key_press
+                self.popups[x].win.process_pointer_enter = self._enter_window
+            self._actually_draw()
+
+    def _actually_draw(self) -> None:
+        # NOTE this function is slow to run and holding down a key can cause lag
+        self._update_time()
+        for x in range(len(qtile.screens)):
+            self.popups[x].clear()
+            # draw image
+            if self.image_path:
+                img = images.Img.from_path(self.image_path)
+                # TODO resize by width or height or only use svg
+                img.resize(height=self.popups[x].height, width=self.popups[x].width)
+                surf, _ = images._decode_to_image_surface(img.bytes_img, img.width, img.height)
+                self.popups[x].draw_image(surf, 0, 0)
+
+            # draw text
+            self.popups[x].text = self.time + self.prompt + self.user_input  # TODO replace user_input with len(user_input) * "*"
+            self.popups[x].draw_text(x=self.text_pos[0], y=self.text_pos[1])
+            self.popups[x].draw()
+
+    def _close(self) -> None:
+        """Close windows and clear variables"""
+        for x in range(len(qtile.screens)):
+            self.popups[x].hide()
+            self.popups[x].kill()
+        self.popups = []
+        self.user_input = ""
+        # TODO refocus previous window
+
+    def _click_popup(self, x, y, button) -> None:
+        if button == 3:
+            self._close()
+
+    def _key_press(self, keycode) -> None:
+        """Handle key press"""
+        # log_test(f"s_l: key press: {keycode}")
+        if keycode == 65293:  # enter
+            if self.user_input == self.fake_password:
+                self._close()
+                return
+            else:
+                self.user_input = ""
+        elif keycode == 65288:  # backspace
+            self.user_input = self.user_input[:-1]
+        elif 31 < keycode < 3900:  # actually typeable characters?
+            self.user_input += chr(keycode)
+        self._actually_draw()
+
+    def _enter_window(self, *args) -> None:
+        """Handle mouse enter window to focus."""
+        self.popups[0].win.focus(warp=False)
+
+    def _update_time(self) -> None:
+        self.time = datetime.now().isoformat()
