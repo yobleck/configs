354718e8b6efa7cec60e89ff38c595dabe9a60aa
yobleck <sennewell@gmail.com>
2024-04-03 19:30:54 -0400

    added simple multi image system to screen locker
    moved image to surface to its own function
    testing some threading stuff
    simplified some loops and renamed some vars

--- qtile/screen_locker.py
+++ qtile/screen_locker.py
@@ -1,6 +1,7 @@
 """Pseudo screen locker that uses Qtile a popup window"""
 
 from datetime import datetime
+from itertools import cycle
 import os
 import threading
 
@@ -16,33 +17,38 @@ def log_test(i):
 
 class lock_screen:  # TODO turn this into an extension?
     def __init__(
-            self, image_path: str = "", background: str = "#000000ff", foreground: str = "#ffffff",
+            self, image_paths: list[str] = [""], background: str = "#000000ff", foreground: str = "#ffffff",
+            image_size: tuple[int, int] = (1080, 1920),  # TODO implement
             font: str = "sans", font_size: int = 16,
-            fake_password: str = "", text_pos: tuple = (0, 0)) -> None:
+            fake_password: str = "", text_pos: tuple = (0, 0),
+            update_interval: float = 1) -> None:
 
-        self.image_path = image_path
-        if self.image_path is None:  # TODO ensure valid image
-            pass
         self.background: str = background
         self.foreground: str = foreground
         self.font: str = font
         self.font_size: int = font_size
 
-        self.time: str = datetime.now().isoformat()
-        self.prompt: str = "\nFake Password:"
+        # text related vars
+        self._time: str = datetime.now().isoformat()
+        self.update_interval = update_interval
+        self.pass_prompt: str = "\nFake Password:"
         self.fake_password: str = fake_password
-        self.user_input: str = ""
+        self._user_input: str = ""
         self.text_pos: tuple = text_pos
 
-        self.popups: list = []
-        self.surface = None
-        if os.path.exists(os.path.expanduser(self.image_path)):  # TODO move this to __init__?
-            img = images.Img.from_path(self.image_path)
-            # TODO resize by width or height or only use svg, or let user decide?
-            img.resize(height=1440, width=2560)  # TODO don't hard code this
-            self.surface, _ = images._decode_to_image_surface(img.bytes_img, img.width, img.height)
-
+        # image related vars
+        self.image_paths: list[str] = image_paths
+        self.popups: list[Popup] = []  # List of popup windows. One per screen
+        # List of all images to be looped through. Not to be confused with different images being displayed on different screens
+        self.surfaces: list = []  # type images._SurfaceInfo
+        self.loop: cycle | None = None
         self.timer: _RepeatTimer | None = None
+
+        # NOTE this takes a really long time with lots of images. try multiprocessing or caching results in pickle?
+        img_thread = threading.Thread(target=self._image_to_surfaces)
+        img_thread.start()  # for now we run in a separate thread to try and deprioritize loading
+        # img_thread.join()
+        # self._image_to_surfaces()
 
     def lock(self, unused_qtile) -> None:
         if not self.popups:
@@ -62,24 +68,41 @@ class lock_screen:  # TODO turn this into an extension?
             self._actually_draw()
 
             try:
-                self.timer = _RepeatTimer(1, self._actually_draw)
+                self.timer = _RepeatTimer(self.update_interval, self._actually_draw)
                 self.timer.start()
             except Exception as e:
                 log_test(e)
 
     def _actually_draw(self) -> None:
-        # NOTE this function is slow to run and holding down a key can cause lag
+        # NOTE this function cant update all screens at the same time so they will appear out of sync
+        # threading or multiprocessing?
         self._update_time()
-        for x in range(len(qtile.screens)):
-            self.popups[x].clear()
+        temp_loop_counter: int = 0
+        if self.surfaces:
+            temp_loop_counter = next(self.loop)  # once per draw cycle so image is the same on every screen
+        for p in self.popups:
+            p.clear()
             # draw image
-            if self.surface:
-                self.popups[x].draw_image(self.surface, 0, 0)
+            if self.surfaces:
+                # pass
+                p.draw_image(self.surfaces[temp_loop_counter], 0, 0)
 
             # draw text
-            self.popups[x].text = self.time + self.prompt + self.user_input  # TODO replace user_input with len(user_input) * "*"
-            self.popups[x].draw_text(x=self.text_pos[0], y=self.text_pos[1])
-            self.popups[x].draw()
+            p.text = self._time + self.pass_prompt + self._user_input  # TODO replace user_input with len(user_input) * "*"
+            p.draw_text(x=self.text_pos[0], y=self.text_pos[1])
+            p.draw()
+
+    def _image_to_surfaces(self) -> None:
+        for img in self.image_paths:
+            if os.path.exists(os.path.expanduser(img)):
+                i_temp = images.Img.from_path(img)
+                # TODO resize by width or height or only use svg, or let user decide?
+                i_temp.resize(height=1440, width=2560)  # TODO don't hard code this
+                surf, _ = images._decode_to_image_surface(i_temp.bytes_img, i_temp.width, i_temp.height)
+                self.surfaces.append(surf)
+            else:
+                log_test(f"invalid image path: {img}")
+        self.loop = cycle(range(len(self.surfaces)))
 
     def _close(self) -> None:
         """Close windows and clear variables"""
@@ -88,11 +111,11 @@ class lock_screen:  # TODO turn this into an extension?
             self.timer = None
         except Exception as e:
             log_test(e)
-        for x in range(len(qtile.screens)):
-            self.popups[x].hide()
-            self.popups[x].kill()
+        for p in self.popups:
+            p.hide()
+            p.kill()
         self.popups = []
-        self.user_input = ""
+        self._user_input = ""
         # TODO refocus previous window
 
     def _click_popup(self, x, y, button) -> None:
@@ -103,26 +126,26 @@ class lock_screen:  # TODO turn this into an extension?
         """Handle key press"""
         # log_test(f"s_l: key press: {keycode}")
         if keycode == 65293:  # enter
-            if self.user_input == self.fake_password:
+            if self._user_input == self.fake_password:
                 self._close()
                 return
             else:
-                self.user_input = ""
+                self._user_input = ""
         elif keycode == 65288:  # backspace
-            self.user_input = self.user_input[:-1]
-        elif 31 < keycode < 3900:  # actually typeable characters?
-            self.user_input += chr(keycode)
-        self._actually_draw()
+            self._user_input = self._user_input[:-1]
+        elif 31 < keycode < 3900:  # most actually typeable characters in this range?
+            self._user_input += chr(keycode)
+        # self._actually_draw()
 
     def _enter_window(self, *args) -> None:
         """Handle mouse enter window to focus."""
         self.popups[0].win.focus(warp=False)
 
     def _update_time(self) -> None:
-        self.time = datetime.now().isoformat()
+        self._time = datetime.now().isoformat()
 
 
 class _RepeatTimer(threading.Timer):
-    def run(self):
+    def run(self) -> None:
         while not self.finished.wait(self.interval):
             self.function(*self.args, **self.kwargs)
