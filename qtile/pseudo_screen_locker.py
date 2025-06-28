da2daacc441737dbe59a321086170add2a772f17
yobleck <sennewell@gmail.com>
2025-06-27 20:33:02 -0400

    add features to pseudo_screen_locker.py (and fix bugs?)
    added installation instructions
    import cairocffi ImageSurface for typing
    import utils for getting qtile cache dir
    added singleton to bypass hook not able to pass through self
    added option to bind lock key from init
    added typing info for ImageSurface
    added lock file so only one instance of psl can run at a time
    disable qtile key binds while lock screen is active
    added arbitrary monitor res support

--- qtile/pseudo_screen_locker.py
+++ qtile/pseudo_screen_locker.py
@@ -1,4 +1,15 @@
-"""Pseudo screen locker that uses Qtile a popup window"""
+"""Pseudo screen locker that uses Qtile a popup window
+Installation:
+Put this file in the same folder as your config file (~/.config/qtile/)
+In the config.py file put:
+import pseudo_screen_locker as psl
+sc = psl.PseudoScreenLocker(
+    image_paths=["/path/to/image.png"],  # make sure this is a string in a list
+    font="sans", font_size=24, foreground="#00aa00", background="#000000aa", text_pos=(1100, 780),
+    key_bind=(["mod4"], "l"),
+    update_interval=1.0)
+# or you can put `Key([mod], "l", lazy.function(sc.lock), desc="lock screen"),` in your config keys list
+"""
 
 from datetime import datetime
 from itertools import cycle
@@ -8,7 +19,9 @@ import threading
 import time
 from typing import Any
 
-from libqtile import hook, images, qtile
+from cairocffi import ImageSurface
+from libqtile import hook, images, lazy, qtile, utils
+from libqtile.config import Key
 from libqtile.popup import Popup
 
 
@@ -18,16 +31,26 @@ def log_test(i: Any):
     f.close()
 
 
-class PseudoScreenLocker:  # TODO turn this into an extension?
+class PseudoScreenLocker:  # TODO turn this into a configurable
+    singleton = None
+
     def __init__(
             self, image_paths: list[str] = [""], background: str = "#000000ff", foreground: str = "#ffffff",
             image_size: tuple[int, int] = (1080, 1920),  # TODO implement
             font: str = "sans", font_size: int = 16,
             fake_password: str = "", text_pos: tuple = (0, 0),
+            key_bind: tuple[list[str], str] | None = None,
             update_interval: float = 1) -> None:
 
+        PseudoScreenLocker.singleton = self
+        self.popups: list[Popup] = []  # List of popup windows. One per screen
         self.background: str = background
         self._backup_key_binds: dict = {}
+        if key_bind:
+            k = Key(*key_bind, lazy.lazy.function(self.lock), desc="lock screen")
+            qtile.keys_map[qtile.core.lookup_key(k)] = k
+            qtile.config.keys.append(k)
+            qtile.core.grab_key(k)
 
         # text related vars
         self.foreground: str = foreground
@@ -42,16 +65,27 @@ class PseudoScreenLocker:  # TODO turn this into an extension?
 
         # image related vars
         self.image_paths: list[str] = image_paths
-        self.popups: list[Popup] = []  # List of popup windows. One per screen
+        self.image_size: tuple[int, int] = image_size
         # List of all images to be looped through.
         # Not to be confused with different images being displayed on different screens
-        self.surfaces: list = []  # type images._SurfaceInfo?
+        self.surfaces: list[ImageSurface] = []  # type images._SurfaceInfo?
         self.loop: cycle | None = None
-        self.temp_loop_counter: int = 0
+        self._temp_loop_counter: int = 0
+
         self.timer: _RepeatTimer | None = None
 
-    def lock(self, unused_qtile) -> None:
+        # remove lock file possibly left over from crash or improper shutdown
+        if os.path.isfile(utils.get_cache_dir() + "/psl_lock"):
+            os.remove(utils.get_cache_dir() + "/psl_lock")
+
+    def lock(self, not_accessed_qtile) -> None:
+        log_test(f"{time.ctime()} locking")
         if not self.popups:
+            with open(utils.get_cache_dir() + "/psl_lock", "w") as f:
+                # create lock file
+                # log_test(utils.get_cache_dir())
+                f.write("p")
+                # pass
             for x, scrn in enumerate(qtile.screens):
                 self.popups.append(Popup(
                     qtile, background=self.background, foreground=self.foreground,
@@ -71,7 +105,7 @@ class PseudoScreenLocker:  # TODO turn this into an extension?
                 self.timer.start()
             except Exception as e:
                 log_test(e)
-            self._backup_key_binds = qtile.keys_map
+            self._backup_key_binds = qtile.keys_map  # TODO pass media keys option
             # log_test(self._backup_key_binds)
             qtile.keys_map = {}
 
@@ -79,45 +113,48 @@ class PseudoScreenLocker:  # TODO turn this into an extension?
         # NOTE this function cant update all screens at the same time so they will appear out of sync
         # threading or multiprocessing?
         self._update_time()
-        if self.surfaces and self.temp_loop_counter:  # don't update background image for typing updates
-            self.temp_loop_counter = next(self.loop)  # only update image once per update_interval so screens are the same
+        if self.surfaces and update_image and self.loop:  # don't update background image for typing updates
+            # only update image once per update_interval so screens are the same
+            self._temp_loop_counter = next(self.loop)
 
         for p in self.popups:
             p.clear()
             # draw image
             if self.surfaces:
                 # pass
-                p.draw_image(self.surfaces[self.temp_loop_counter], 0, 0)
+                p.draw_image(self.surfaces[self._temp_loop_counter], 0, 0)
 
             # draw text
-            p.text = self._time + self.pass_prompt + self._user_input  # TODO replace user_input with len(user_input) * "*"
+            p.text = self._time + self.pass_prompt + len(self._user_input) * "*"
             p.draw_text(x=self.text_pos[0], y=self.text_pos[1])
             p.draw()
 
     def load_images(self) -> None:
-        # NOTE this takes a really long time with lots of images. try caching results in pickle?
+        # NOTE this takes a really long time with lots of images.
         img_thread = threading.Thread(target=self._images_to_surfaces)
-        img_thread.start()  # for now we run in a separate thread to try and deprioritize loading
+        img_thread.start()  # for now we run in a separate thread to try and de-prioritize loading
         # img_thread.join()
 
     def _images_to_surfaces(self) -> None:
-        time.sleep(0.1)  # deprioritize thread?
+        time.sleep(0.1)  # de-prioritize thread?
         for img in self.image_paths:
             if os.path.exists(os.path.expanduser(img)):
                 i_temp = images.Img.from_path(img)
-                # TODO resize by width or height or only use svg, or let user decide?
-                i_temp.resize(height=1440, width=2560)  # TODO don't hard code this
-                surf, _ = images._decode_to_image_surface(i_temp.bytes_img, i_temp.width, i_temp.height)
+                # TODO resize by width or height or only use SVG, or let user decide?
+                i_temp.resize(height=self.image_size[0], width=self.image_size[1])
+                surf, _ = images.get_cairo_surface(i_temp.bytes_img, i_temp.width, i_temp.height)
                 self.surfaces.append(surf)
             else:
-                log_test(f"invalid image path: {img}")
+                log_test(f"{datetime.now()} invalid image path: {img}")
         self.loop = cycle(range(len(self.surfaces)))
 
     def _close(self) -> None:
         """Close windows and clear variables"""
-        qtile.keys_map = self._backup_key_binds  # TODO sometimes doesn't work. WHY?
+        qtile.keys_map = self._backup_key_binds  # BUG sometimes doesn't work. WHY? screen still off? dpms
+        qtile.grab_keys()  # does this fix the bug above?
         try:
-            self.timer.cancel()  # breaks if lock immediately after qtile loads
+            if self.timer:  # just in case the function is run while already closed
+                self.timer.cancel()  # BUG breaks if lock immediately after qtile loads
             self.timer = None
         except Exception as e:
             log_test(e)
@@ -126,9 +163,10 @@ class PseudoScreenLocker:  # TODO turn this into an extension?
             p.kill()
         self.popups = []
         self._user_input = ""
+        os.remove(utils.get_cache_dir() + "/psl_lock")
         # TODO refocus previous window
 
-    def _click_popup(self, x, y, button) -> None:
+    def _click_popup(self, x, y, button) -> None:  # TODO remove this
         if button == 3:
             self._close()
 
@@ -142,15 +180,17 @@ class PseudoScreenLocker:  # TODO turn this into an extension?
             else:
                 self._user_input = ""
         elif keycode == 65288:  # backspace
-            self._user_input = self._user_input[:-1]
-        elif 31 < keycode < 3900:  # most actually typeable characters in this range?
+            self._user_input = self._user_input[:-1]  # pop() faster?
+        elif 31 < keycode < 3900:  # most actually type able characters in this range?
             self._user_input += chr(keycode)
         self._actually_draw(update_image=False)
 
-    def fire_hook(self) -> None:
+    @staticmethod
+    @hook.subscribe.user("psl_lock_hook")
+    def fire_hook():
         """qtile cmd-obj -o cmd -f fire_user_hook -a 'custom_hook_name'"""
-        # log_test("hook fired")
-        self.lock(qtile)
+        # NOTE This 'singleton' is required to bypass the hook not being able to pass self to the function
+        PseudoScreenLocker.singleton.lock(qtile)
 
     def _enter_window(self, *args) -> None:
         """Handle mouse enter window to focus."""
