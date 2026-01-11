b1129b6e41a24fb1178b0dd581954d6259f3c6fb
yobleck <sennewell@gmail.com>
2026-01-11 12:40:58 -0500

    Update locker.sh
    switched from i3lock to my custom built into qtile screen locker

--- qtile/locker.sh
+++ qtile/locker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #echo auto >> /home/yobleck/xtest.txt
-#if [ $(cut -f1 -d. /proc/uptime) -lt 500 ];
-#then
-if [ $(grep -r "RUNNING" /proc/asound | wc -l) -eq 0 ] && [ $(cut -f1 -d. /proc/uptime) -gt 500 ];
+
+# check for sound playing, computer has been on for more than 5 min, screen not already locked
+if [ $(grep -r "RUNNING" /proc/asound | wc -l) -eq 0 ] && \
+   [ $(cut -f1 -d. /proc/uptime) -gt 300 ] && \
+   [ ! -f ~/.cache/qtile/psl_lock ];
 then
-xset s activate
-i3lock -n --pass-media-keys --time-color=00ff00 --date-color=00aa00 --blur 5 --clock --time-str="%H:%M" --date-str="%Y-%m-%d"
-xset s off
+    echo "$(date) xautolock triggered" >> ~/.config/qtile/qtile_log.txt
+    xset s activate
+    #i3lock -n --pass-media-keys --time-color=00ff00 --date-color=00aa00 --blur 5 --clock --time-str="%H:%M" --date-str="%Y-%m-%d"
+    qtile cmd-obj -o cmd -f fire_user_hook -a psl_lock_hook &>> /dev/null #~/temp_buffer.txt
+    xset s off
 fi
-#fi
